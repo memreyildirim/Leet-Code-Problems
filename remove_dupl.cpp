7f8f4fa4122238094486b950f22c7420d929ed6d
Mustafa Emre Yıldırım <yildirimmus16@itu.edu.tr>
2020-11-23 20:26:46 +0300

    commit-Nov23

--- remove_dupl.cpp
+++ remove_dupl.cpp
@@ -28,9 +28,8 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         vector<int> ret;
-        for (int i=0;i<nums.size(); i++){
-    
-            if(i==0){
+        for (int i=0;i<nums.size(); i++){    
+           if(i==0){
                 ret.push_back(nums[i]);
             }
             else{
